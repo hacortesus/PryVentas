d039e38f0f6e2525876d1635bb0ed24221985c56
hacortesus <hacortesus@gmail.com>
2023-06-13 13:17:56 -0400

    se modifica test2

--- test2.cbl
+++ test2.cbl
@@ -1,8 +1,8 @@
        identification division.
-       program-id. prueba.
+       program-id. test2.
        data division.
        working-storage section.
        procedure division.
        partir.
-           display "buenas noches".
+           display "modificacion1 de test2.cbl".
            stop run.
